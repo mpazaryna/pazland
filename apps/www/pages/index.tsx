c4c7e087e445395075f8594d3a6080f025ade439
mpazaryna <mpazbot@gmail.com>
2022-07-24 17:29:09 -0400

    added shared-ui footer and header

--- apps/www/pages/index.tsx
+++ apps/www/pages/index.tsx
@@ -1,4 +1,6 @@
 import styles from './index.module.scss';
+import { Header } from '@pazland/shared-ui';
+import { Footer } from '@pazland/shared-ui';
 
 export function Index() {
   /*
@@ -11,10 +13,7 @@ export function Index() {
       <div className="wrapper">
         <div className="container">
           <div id="welcome">
-            <h1>
-              <span> Hello there, </span>
-              Welcome www 👋
-            </h1>
+            <Header>Welcome to my website</Header>
           </div>
 
           <div id="hero" className="rounded">
@@ -390,22 +389,9 @@ export function Index() {
             </details>
           </div>
 
-          <p id="love">
-            Carefully crafted with
-            <svg
-              fill="currentColor"
-              stroke="none"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-              />
-            </svg>
-          </p>
+          <div id="footer">
+            <Footer />
+          </div>
         </div>
       </div>
     </div>
---
+++ libs/shared-ui/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/header/header';
+export * from './lib/footer/footer';
+export * from './lib/shared-ui';
